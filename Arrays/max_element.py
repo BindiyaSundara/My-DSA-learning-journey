45a7a2e78987a70f29e9725bc8d6d8b96e336ad3
BindiyaSundara <bindiyasundara93@gmail.com>
2026-01-29 18:37:24 +0530

    Added max element program

---
+++ Arrays/max_element.py
@@ -0,0 +1,9 @@
+def find_max(arr):
+    max_element = arr[0]
+    for i in range(1, len(arr)):
+        if arr[i] > max_element:
+            max_element = arr[i]
+    return max_element
+
+arr = [3, 7, 2, 9, 5]
+print("Maximum element is:", find_max(arr))
